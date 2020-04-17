709d6c9c51722d4c0bb09def0baf5321b8a3ec80
Dehvon Curtis <dcurtis@adobe.com>
2020-04-17 15:38:18 -0600

    motorcycles.py complete

---
+++ Chapter_3/motorcycles.py
@@ -0,0 +1,31 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'kawasaki']
+print('Original: ', motorcycles)
+
+#replacing
+# motorcycles[0] = 'ducati'
+
+#appending elements
+# motorcycles.append('harley'))
+
+#inserting elements
+# motorcycles.insert(1, 'Harley')
+
+# removing elements
+# del motorcycles[1]
+
+# Removing an Item Using the pop() Method
+# popped_motorcycle = motorcycles.pop()
+# print('Popped: ', popped_motorcycle)
+
+# Popping Items from any Position in a List
+# first_owned = motorcycles.pop(0)
+# print('First owned: ', first_owned.title())
+
+# Removing an Item by Value
+# motorcycles.remove('yamaha')
+# too_expensive = 'suzuki'
+# motorcycles.remove(too_expensive)
+
+
+
+print('New: ', motorcycles)

341385505759349dd55d75054a6ca92825cc82a7
Dehvon Curtis <dcurtis@adobe.com>
2020-04-20 15:54:28 -0600

    magicians.py

---
+++ Chapter_4/magicians.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+
+for m in magicians:
+    print(f'Wow {m.title()}, you performed a great trick')
+    print(f'I can\'t wait for your next trick {m.title()}\n')
+
+print('Thanks everyone, that was a great magic show')

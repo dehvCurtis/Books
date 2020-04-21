a89f3c192334e89f528e1fad2c833d61d0d3e9f0
Dehvon Curtis <dcurtis@adobe.com>
2020-04-21 11:26:33 -0600

    cars.py

--- Chapter_5/cars.py
+++ Chapter_5/cars.py
@@ -1,3 +1,9 @@
 cars = ['audi', 'bmw', 'subaru', 'toyota', 'gmc']
 
 for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    elif car == 'gmc':
+        print(car.upper())
+    else:
+        print(car.title())

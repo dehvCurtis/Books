8f8f9f04962af402b69c5003c7d1df8a297e84d9
Dehvon Curtis <dcurtis@adobe.com>
2020-04-22 12:17:37 -0600

    completed 5-*.py

--- Chapter_5/toppings.py
+++ Chapter_5/toppings.py
@@ -3,13 +3,33 @@
 # if requested_topping != 'anchovies':
 #     print('Hold the anchovies')
 
-requested_topping = ['mushrooms','olives']
+# requested_topping = ['mushrooms','olives']
+#
+# if 'mushrooms' in requested_topping:
+#     print('Adding mushrooms')
+# elif 'pepperoni' in requested_topping:
+#     print('Adding pep')
+# elif 'extra cheese' in requested_topping:
+#     print('Extra cheese')
+#
+# print('Finished making pizza')
 
-if 'mushrooms' in requested_topping:
-    print('Adding mushrooms')
-elif 'pepperoni' in requested_topping:
-    print('Adding pep')
-elif 'extra cheese' in requested_topping:
-    print('Extra cheese')
+# Checking for Special Items
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+#
+# for topping in requested_toppings:
+#     if topping == 'green peppers':
+#         print(f'Out of {topping}')
+#     else:
+#         print(f'Adding {topping}')
+#
+# print('All done')
 
-print('Finished making pizza')
+# Checking That a List Is Not Empty
+requested_toppings = []
+
+if requested_toppings:
+    for topping in requested_toppings:
+        print(f'Adding {topping}')
+else:
+    print('Are you sure you want a plain pizza?')

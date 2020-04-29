983718f1cd135df6096eefe337a67573204c63a7
Dehvon Curtis <dcurtis@adobe.com>
2020-04-29 11:47:01 -0600

    created pets.py

---
+++ Chapter_8/pets.py
@@ -0,0 +1,17 @@
+# Positional Arguments
+# def describe_pet(animal_type, ainmal_name):
+#     """ DISPLAY INFO ABOUT PET """
+#     print(f'\nAnimal Type: {animal_type.title()}')
+#     print(f'Animal Name: {ainmal_name.title()}')
+#
+# describe_pet('Dog','Dakota')
+# describe_pet('Cat','Owen')
+
+# Keyword Arguments
+def describe_pet(animal_type, ainmal_name):
+    """ DISPLAY INFO ABOUT PET """
+    print(f'\nAnimal Type: {animal_type.title()}')
+    print(f'Animal Name: {ainmal_name.title()}')
+
+describe_pet('Dog','Dakota')
+describe_pet('Cat','Owen')

43a904264bca88b1389df6a0147842167e96293b
Dehvon Curtis <dcurtis@adobe.com>
2020-05-06 11:12:57 -0600

    added def()

--- Chapter_8/greeter.py
+++ Chapter_8/greeter.py
@@ -18,9 +18,13 @@ def get_formatted_name(first_name,last_name):
     return full_name.title()
 
 while True:
-    print('\nPlease type your name.')
+    print('\nPlease type your name or \'quit\'')
     fname = input('First Name > ')
+    if fname == 'quit':
+        break
     lname = input('Last Name > ')
+    if lname == 'quit':
+        break
 
     formatted_name = get_formatted_name(fname,lname)
     print(f'Hello {formatted_name}')

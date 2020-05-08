38ffd2be18cef2f8ec1d93c5e2b13894348e54cf
Dehvon Curtis <dcurtis@adobe.com>
2020-05-08 10:27:23 -0600

    printing_models.py complete

---
+++ Python3/Python_Crash_Course-2nd/Chapter_8/printing_functions.py
@@ -0,0 +1,11 @@
+
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f'Model: {current_design.title()}')
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    for i in completed_models:
+        print(f'Completed Models: {i.title()}')
+
--- Python3/Python_Crash_Course-2nd/Chapter_8/printing_models.py
+++ Python3/Python_Crash_Course-2nd/Chapter_8/printing_models.py
@@ -1,17 +1,21 @@
 
-def print_models(unprinted_designs, completed_models):
-    while unprinted_designs:
-        current_design = unprinted_designs.pop()
-        print(f'Model: {current_design.title()}')
-        completed_models.append(current_design)
+import printing_functions as prnt_func
 
-def show_completed_models(completed_models):
-    for i in completed_models:
-        print(f'Completed Models: {i.title()}')
+# def print_models(unprinted_designs, completed_models):
+#     while unprinted_designs:
+#         current_design = unprinted_designs.pop()
+#         print(f'Model: {current_design.title()}')
+#         completed_models.append(current_design)
+#
+# def show_completed_models(completed_models):
+#     for i in completed_models:
+#         print(f'Completed Models: {i.title()}')
 
+incomplete_designs = ['Tiger','Elephant','Gorilla']
+complete_designs = []
 
-unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = []
+# print_models(unprinted_designs, completed_models)
+# show_completed_models(completed_models)
 
-print_models(unprinted_designs, completed_models)
-show_completed_models(completed_models)
+prnt_func.print_models(incomplete_designs,complete_designs)
+prnt_func.show_completed_models(complete_designs)

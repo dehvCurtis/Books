228c3912c79b57b6baafeca04b3fa55832fc9172
Dehvon Curtis <dcurtis@adobe.com>
2020-05-13 11:42:27 -0600

    starting Ch9

--- Python3/Python_Crash_Course-2nd/Chapter_9/dog.py
+++ Python3/Python_Crash_Course-2nd/Chapter_9/dog.py
@@ -12,3 +12,12 @@ class Dog:
 
     def rollover(self):
         print(f'{self.name} is rolling over')
+
+
+my_dog = Dog('Tido',3)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+
+your_dog = Dog('Kitty',5)
+print(f"Your dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")

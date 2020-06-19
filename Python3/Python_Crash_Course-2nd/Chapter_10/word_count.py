377af8c95de0a45eabd6bc59eb63232c6e16b555
Dehvon Curtis <dcurtis@adobe.com>
2020-06-19 16:01:10 -0600

    added text formatting

--- Python3/Python_Crash_Course-2nd/Chapter_10/word_count.py
+++ Python3/Python_Crash_Course-2nd/Chapter_10/word_count.py
@@ -4,13 +4,13 @@ def count_words(filename):
         with open(filename, encoding='utf-8') as file:
             contents = file.read()
     except FileNotFoundError:
-        print('File not found!')
+        print(f'File {filename} not found!')
     else:
         words = contents.split()
         num_words = len(words)
         print(f'The file {filename} has appx {num_words} number of words')
 
-filenames = ['alice.txt','Pride_and_Prejudice.txt']
+filenames = ['alice.txt','Pride_and_Prejudice.txt','Frankenstein.txt']
 
 for i in filenames:
     count_words(i)

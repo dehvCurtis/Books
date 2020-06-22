f2a22b27b942de2f6adf2cc2790fe3d81912ea44
Dehvon Curtis <dcurtis@adobe.com>
2020-06-22 15:40:04 -0600

    added func()

--- Python3/Python_Crash_Course-2nd/Chapter_10/remember_me.py
+++ Python3/Python_Crash_Course-2nd/Chapter_10/remember_me.py
@@ -2,14 +2,17 @@ import json
 
 filename = 'username1.json'
 
-try:
-    with open(filename) as file:
-        f = json.load(file)
-        print(f'Welcome back {f}')
+def greeter():
+    try:
+        with open(filename) as file:
+            f = json.load(file)
+            print(f'Welcome back {f}')
 
-except FileNotFoundError:
-    print('file not found')
-    username = input('What is your name > ')
-    with open(filename, 'w') as file:
-        json.dump(username, file)
-        print(f'Registered user: {username}')
+    except FileNotFoundError:
+        print('file not found')
+        username = input('What is your name > ')
+        with open(filename, 'w') as file:
+            json.dump(username, file)
+            print(f'Registered user: {username}')
+
+greeter()

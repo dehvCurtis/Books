b8e3f432be0a44ef85945d4cb10076225cbb7804
Dehvon Curtis <dcurtis@adobe.com>
2020-06-30 19:56:29 -0600

    web server template

--- TerraForm/Terraform_Up_-_Running-2nd/Chapter 2/main.tf
+++ TerraForm/Terraform_Up_-_Running-2nd/Chapter 2/main.tf
@@ -3,6 +3,34 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-026dea5602e368e96"
+  ami = "ami-0a63f96e85105c6d3"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+# security group
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instace"
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "http port 8080"
+  type = number
+  default = 8080
 }

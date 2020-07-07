2346d92770aa12a7ac7399320760befde41ea86d
Dehvon Curtis <dcurtis@adobe.com>
2020-07-07 09:56:01 -0600

    commit

---
+++ Python3/Python_Crash_Course-2nd/Chapter_12/alien_invasion/alien_invasion.py
@@ -0,0 +1,31 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """ Overall class to manage game assets and behavaior """
+
+    def __init__(self):
+        """ initializes game and creates game resources """
+        pygame.init()
+
+        # Background Color
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption('Alien INVASION!!!')
+
+    def run_game(self):
+        """ start main loop of game """
+        while True:
+            # watch for keyboard / mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # make the most recently drawn screen visible
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance and run game
+    AlienInvasion = AlienInvasion()
+    AlienInvasion.run_game()
--- TerraForm/Terraform_Up_-_Running-2nd/Chapter 2/main.tf
+++ TerraForm/Terraform_Up_-_Running-2nd/Chapter 2/main.tf
@@ -2,18 +2,35 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0a63f96e85105c6d3"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0a63f96e85105c6d3"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  tags = {
-    Name = "terraform-example"
+  # Required when using a launch configuration with an auto scaling group.
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# auto-scaling group (ASG)
+resource "aws_autoscaling_group" "asg_example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  max_size = 4
+  min_size = 2
+
+  tag {
+    key = "Name"
+    propagate_at_launch = true
+    value = "terraform-asg-example"
   }
 }
 
@@ -42,4 +59,13 @@ output "public_ip" {
 
 output "arn_id" {
   value = aws_instance.example.arn
+}
+
+# data source
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_vpc" "default1" {
+  vpc_id = data.aws_vpc.default.id
 }

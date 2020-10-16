e18e51ee00911550a6350604142a4c43fcaee639
Dehvon Curtis <dcurtis@berkadia.com>
2020-10-16 13:12:43 -0600

    restarting book

--- Python3/Python for DevOps/Ch_01/ex3.py
+++
@@ -1,6 +0,0 @@
-# Write a list comprehension that results in a list of every letter in the word smogtether capitalized.
-
-string = 'smogtether'
-new_list = []
-list = [new_list.append(letter) for letter in string]
-print(new_list)

c080495dd243b8a0ca393507f7b1e1a511e30ab9
Dehvon Curtis <dcurtis@berkadia.com>
2020-12-15 12:35:22 -0700

    ex1 complete

---
+++ Python3/Python_for_DevOps/Ch_01/ex1.py
@@ -0,0 +1,9 @@
+# Python for DevOps
+# Chapter 1
+
+# Write a Python function that takes a name as an argument and prints that name.
+
+def print_name(name):
+    print(f'Hello {name}')
+
+print_name('Jake')

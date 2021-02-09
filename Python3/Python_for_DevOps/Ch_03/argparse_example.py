92f7036d15509763ea30f9031c0364a5c745248b
Dehvon Curtis <dcurtis@berkadia.com>
2021-02-09 13:36:42 -0700

    finished script

--- Python3/Python_for_DevOps/Ch_03/argparse_example.py
+++ Python3/Python_for_DevOps/Ch_03/argparse_example.py
@@ -3,15 +3,15 @@
 import argparse
 
 def sail():
-    ship_name = 'Tallihoe'
+    ship_name = 'Your ship'
     print(f'{ship_name} is setting sail')
 
 def list_ships():
-    ships = ['John B','Yankee','Pequod','Maria']
-    print(f'Ships: {ships}')
+    ships = ['John B','Yankee Clipper','Pequod']
+    print(f'Ships: {",".join(ships)}')
 
-def greet(greeting,name):
-    message = f'{greeting} {name}'
+def greet():
+    message = f'{greeting}{name}'
     print(message)
 
 if __name__ == '__main__':
@@ -21,4 +21,17 @@ if __name__ == '__main__':
 
     subparsers = parser.add_subparsers(dest='func')
 
-    
+    ship_parser = subparsers.add_parser('ships', help='Ship related commands')
+    ship_parser.add_argument('command', choices=['list','sail'])
+
+    sailor_parser = subparsers.add_parser('sailors', help='Talk to the sailor')
+    sailor_parser.add_argument('name', help='Sailors name')
+    sailor_parser.add_argument('--greeting','-g', help='Greeting', default='Ahoy there')
+
+    args = parser.parse_args()
+    if args.func == 'sailors':
+        greet(args.greeting, args.name)
+    elif args.command == 'list':
+        list_ships()
+    else:
+        sail()

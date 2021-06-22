14b1f6b00a7d02239fda85665473df7b4e8cc650
Dehvon Curtis <dcurtis@berkadia.com>
2021-06-22 13:39:56 -0600

    updated

--- Python3/Learn-Python3-The-Hard-Way/ex38.py
+++ Python3/Learn-Python3-The-Hard-Way/ex38.py
@@ -1,20 +1,14 @@
-ten_things = "Apples Oranges Crows Phones Light Sugar"
+ten_things = "Apples Oranges Crows Telephone Computer"
 
-print("Wait there aren\'t ten things in that list.")
+print('Wait there are not 10 things, let\'s fix that')
 
-stuff = ten_things.split(' ')
-more_stuff = ["Night","Day","Entry","Test"]
+stuff = ten_things.split()
+more_stuff = ['Day','Night','Shovel','Dog','Cat','Book','TV']
 
-while len(stuff) != 10:
+while len(ten_things) != 10:
     next_one = more_stuff.pop()
-    print(f"Adding {next_one}")
+    print(f'Adding: {next_one}')
     stuff.append(next_one)
-    print(f'There are {len(stuff)} items now')
+    print(f'There are {len(stuff)} now')
 
-print(f"There we go: {stuff}")
-
-print(stuff[1])
-print(stuff[-1])
-print(stuff.pop())
-print(' '.join(stuff))
-print('#'.join(stuff[3:5]))
+print("There we go: ", stuff)

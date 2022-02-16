6fda83226b39c0eb6846e39c8f4662d73aa32fa0
Dehvon Curtis <dcurtis@datto.com>
2022-02-16 15:17:11 -0700

    commit

---
+++ Python3/Learn-Python3-The-Hard-Way/ex17.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f'Copying file: {from_file} to file: {to_file}')
+
+in_file = open(from_file)
+indata = in_file.read()
+
+print(f'The input file is {len(indata)} bytes long')
+
+print(f'Does the output file exist? {exists(to_file)}')
+print('Ready, hit RETURN to continue')
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print('Complete')
+
+out_file.close()
+in_file.close()

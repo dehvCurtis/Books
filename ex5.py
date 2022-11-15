7a2a7090ac0775d14dff0f8557cf69c5de3cfbc6
dcurtis@datto.com <dcurtis@datto.com>
2022-11-15 08:25:53 -0700

    ex5 updated with study drills

--- ex5.py
+++ ex5.py
@@ -1,13 +1,13 @@
-my_name = 'John Snow'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'John Snow'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print(f'Let\'s talk about {my_name}')
-print(f'He\'s {my_height} inches tall')
-print(f'He is {my_weight} pounds')
-print(f'He\'s got {my_eyes} eyes and {my_hair} hair')
-print(f'His teeth are usually {my_teeth} depending on coffee')
+print(f'Let\'s talk about {name}')
+print(f'He\'s {height} inches tall')
+print(f'He is {weight} pounds')
+print(f'He\'s got {eyes} eyes and {hair} hair')
+print(f'His teeth are usually {teeth} depending on coffee')

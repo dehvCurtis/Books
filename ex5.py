1350bb4ff7a6a6af90dd7921fff71cff389204ff
dcurtis@datto.com <dcurtis@datto.com>
2022-11-15 08:29:59 -0700

    ex5 updated with study drills

--- ex5.py
+++ ex5.py
@@ -1,7 +1,9 @@
 name = 'John Snow'
 age = 35 # not a lie
 height = 74 # inches
+height_cm = height * 2.54
 weight = 180 # lbs
+weight_kilo = weight * 0.45359237
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
@@ -11,3 +13,6 @@ print(f'He\'s {height} inches tall')
 print(f'He is {weight} pounds')
 print(f'He\'s got {eyes} eyes and {hair} hair')
 print(f'His teeth are usually {teeth} depending on coffee')
+
+print(f'My height in cm: {height_cm}')
+print(f'My weight in kilos: {weight_kilo}')

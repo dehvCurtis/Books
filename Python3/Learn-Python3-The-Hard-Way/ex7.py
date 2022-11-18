bc0ee58ffc11e32328aa623aade67a436f9d945c
dcurtis@datto.com <dcurtis@datto.com>
2022-11-18 08:33:25 -0700

    fixed typos

--- Python3/Learn-Python3-The-Hard-Way/ex7.py
+++ Python3/Learn-Python3-The-Hard-Way/ex7.py
@@ -3,15 +3,18 @@ print("Its fleece was white as {}.".format('snow'))
 print("And everywhere that Mary went")
 print("." * 10)
 
-6 end1 = "C"
-7 end2 = "h"
-8 end3 = "e"
-9 end4 = "e"
-10 end5 = "s"
-11 end6 = "e"
-12 end7 = "B"
-13 end8 = "u"
-14 end9 = "r"
-15 end10 = "g"
-16 end11 = "e"
-17 end12 = "r"
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)

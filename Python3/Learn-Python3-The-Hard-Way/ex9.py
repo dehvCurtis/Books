baf7b942a2d14e45212c238decd458baa1db2285
dcurtis@datto.com <dcurtis@datto.com>
2022-11-21 08:19:01 -0700

    commit

--- Python3/Learn-Python3-The-Hard-Way/ex9.py
+++ Python3/Learn-Python3-The-Hard-Way/ex9.py
@@ -5,9 +5,9 @@ print('Here are the days: ', days)
 print('Here are the months: ', months)
 
 print('''
-    There\'s something going on here.
-    With the three quotes
-    We\'ll be able to type a lot
-    Even 4 lines
-    If we want, 5, 6 or 7
+There\'s something going on here.
+With the three quotes
+We\'ll be able to type a lot
+Even 4 lines
+If we want, 5, 6 or 7
 ''')

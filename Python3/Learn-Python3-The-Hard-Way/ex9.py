6d2011b36205bf5110a91e532989bdf6f37138dd
dcurtis@datto.com <dcurtis@datto.com>
2022-11-21 08:18:44 -0700

    commit

--- Python3/Learn-Python3-The-Hard-Way/ex9.py
+++ Python3/Learn-Python3-The-Hard-Way/ex9.py
@@ -1,1 +1,13 @@
-#
+days = "Mon Tues Wed Thurs Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print('Here are the days: ', days)
+print('Here are the months: ', months)
+
+print('''
+    There\'s something going on here.
+    With the three quotes
+    We\'ll be able to type a lot
+    Even 4 lines
+    If we want, 5, 6 or 7
+''')

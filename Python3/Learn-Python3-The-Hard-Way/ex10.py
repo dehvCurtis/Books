153205c7fc0d786e3fd97a95ce907cb1540b32d0
dcurtis@datto.com <dcurtis@datto.com>
2022-11-22 08:18:42 -0700

    commit

---
+++ Python3/Learn-Python3-The-Hard-Way/ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\t I'm tabbed in"
+persian_cat = "I'm split\non a line"
+backslack_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll make a list:
+\t* Cat food
+\t* Fish
+\t* Catnip\n\t*Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
